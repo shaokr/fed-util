e4b3e7c1b845f20fc774feac98c59c6645be1318
shaokr <shaokrg@gamil.com>
2020-09-07 15:07:39 +0800

    0.0.9

--- dist/param.js
+++ dist/param.js
@@ -1,22 +1,26 @@
+var _a;
 /**
  * 解析参数等
  * shaokr 2020.8.18
  */
 import qs from "qs";
+import { get } from "./global-var";
 /**
  * 传入url可解析url中的参数
  */
 export var getParam = function (url) {
-    var search = url.match(/\?([^#]+)/);
-    if (search) {
-        var _search = search[1];
-        return qs.parse(_search);
+    if (url) {
+        var search = url.match(/\?([^#]+)/);
+        if (search) {
+            var _search = search[1];
+            return qs.parse(_search);
+        }
     }
     return {};
 };
 // 输出给外面的param处理
-var param = getParam(location.href);
-var _debug = param.debug || window.debug;
+var param = getParam((_a = get("location")) === null || _a === void 0 ? void 0 : _a.href);
+var _debug = param.debug || get("debug");
 if (typeof _debug !== "undefined") {
     param.debug = _debug;
 }
--- src/param.ts
+++ src/param.ts
@@ -3,21 +3,24 @@
  * shaokr 2020.8.18
  */
 import qs from "qs";
+import { get } from "./global-var";
 /**
  * 传入url可解析url中的参数
  */
 export const getParam = (url: string): { [key: string]: any } => {
-  let search = url.match(/\?([^#]+)/);
-  if (search) {
-    let _search = search[1];
-    return qs.parse(_search);
+  if (url) {
+    let search = url.match(/\?([^#]+)/);
+    if (search) {
+      let _search = search[1];
+      return qs.parse(_search);
+    }
   }
   return {};
 };
 
 // 输出给外面的param处理
-const param = getParam(location.href);
-const _debug = param.debug || window.debug;
+const param = getParam(get("location")?.href);
+const _debug = param.debug || get("debug");
 if (typeof _debug !== "undefined") {
   param.debug = _debug;
 }
