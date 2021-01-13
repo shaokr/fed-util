9315bad0e857108b7e4c7553118758668f01fb01
shaokr <shaokangrun@songshuai.com>
2021-01-13 11:09:19 +0800

    up

--- src/promise-overtime.ts
+++ src/promise-overtime.ts
@@ -1,8 +1,23 @@
-export default (promise: Promise<any>, time: number = 10000, data: any = false) => {
+const promiseOvertime = (
+  promise: Promise<any>,
+  time: number = 10000,
+  data: any = false
+) => {
   const overtime = new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(data)
-    }, time)
-  })
-  return Promise.race([overtime, promise])
-}
+      resolve(data);
+    }, time);
+  });
+  return Promise.race([overtime, promise]);
+};
+export const promiseOvertimeApi = (
+  promise: Promise<any>,
+  time: number = 10000,
+  data: any = {
+    isSuccess: false,
+    message: "连接超时",
+  }
+) => {
+  return promiseOvertime(promise, time, data);
+};
+export default promiseOvertime;
